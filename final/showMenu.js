7d2190da59a2ff170edfdfb937b0c4758bc6c50f
oykupacurlu <143540159+oykupacurlu@users.noreply.github.com>
2023-12-07 17:57:19 +0000

    final

---
+++ final/showMenu.js
@@ -0,0 +1,9 @@
+function showMenu(){
+    var  menu = document.getElementById("topmenu");
+    if  (menu.style.display==="none"){
+        menu.style.display="block";
+    } else {
+    menu.style.display="none";
+        }
+    }
+    
